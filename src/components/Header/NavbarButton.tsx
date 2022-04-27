0a9d000b7ff162e760c39d60520bae24b0c2e57a
Jack Li <jack@jackli.dev>
2022-04-26 23:12:00 -0700

    feat: navbar routes with next/link

--- src/components/Header/NavbarButton.tsx
+++ src/components/Header/NavbarButton.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const NavbarButton = (props: any) => {
     return (
-        <a href={props.link} className="text-norm-gray text-[16px] font-firamono underline underline-offset-4 mx-[10px]">
-            {props.label}
-        </a>
+        <Link href={props.link}>
+            <a className="text-norm-gray text-[16px] font-firamono underline underline-offset-4 mx-[10px]" onClick={props.onClick}>
+                {props.label}
+            </a>
+        </Link>
     )
 }
