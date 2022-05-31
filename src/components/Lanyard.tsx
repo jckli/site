ca79197d0896d490ce4d9dba0a1f9a66be397c47
lina <jack@jackli.dev>
2022-05-31 06:55:36 +0000

    fix: lanyard custom status

--- src/components/Lanyard.tsx
+++ src/components/Lanyard.tsx
@@ -5,13 +5,17 @@ import { LanyardData } from "../hooks/LanyardData";
 
 export const Lanyard = (props: any) => {
     const activity = LanyardData();
+    var anum = 0
+    if (activity?.activities[0].type == 4) {
+        anum = 1
+    }
     return (
         <div>
-            {activity?.activities[0] ? (
+            {activity?.activities[anum] ? (
                 <div className="block lg:absolute lg:top-0 lg:right-0 text-white lg:pr-[10px] lg:pt-[10px] max-w-[350px]">
                     <div className="rounded-lg bg-[#303030]/50">
                         <div className="py-[15px] px-[25px]">
-                            {activity?.activities[0].type == 2 ? (
+                            {activity?.activities[anum].type == 2 ? (
                                 <div>
                                     <h1 className="font-firamono text-sm font-bold text-[#cccccc]">
                                         Listening to Spotify
@@ -21,10 +25,10 @@ export const Lanyard = (props: any) => {
                                         {activity?.spotify?.album_art_url ? (
                                             <div className="relative h-[50px] mr-4">
                                                 <div className="min-w-[50px] h-[50px] relative rounded-[8px] overflow-hidden">
-                                                    <Image alt={`${activity?.activities[0].name} Large Image`} src={activity.spotify!.album_art_url} layout="fill" />
+                                                    <Image alt={`${activity?.activities[anum].name} Large Image`} src={activity.spotify!.album_art_url} layout="fill" />
                                                 </div>
                                                 <div className="absolute bottom-[-5px] right-[-5px] min-w-[20px] h-[20px] rounded-[50%] bg-black border-2 border-solid border-black/80 overflow-hidden">
-                                                    <Image alt={`${activity?.activities[0].name} Small Image`} src={SpotifyLogo} layout="fill" />
+                                                    <Image alt={`${activity?.activities[anum].name} Small Image`} src={SpotifyLogo} layout="fill" />
                                                 </div>
                                             </div>
                                         ) : (
@@ -44,14 +48,14 @@ export const Lanyard = (props: any) => {
                                         Doing something
                                     </h1>
                                     <div className="flex items-center mt-2 ">
-                                        {activity?.activities[0].assets ? (
+                                        {activity?.activities[anum].assets ? (
                                         <div className="relative h-[50px] mr-4">
                                             <div className="min-w-[50px] h-[50px] relative rounded-[8px] overflow-hidden">
-                                                <Image alt={`${activity?.activities[0].name} Large Image`} src={`https://cdn.discordapp.com/app-assets/${activity.activities[0].application_id}/${activity.activities[0].assets!.large_image}.png`} layout="fill" />
+                                                <Image alt={`${activity?.activities[anum].name} Large Image`} src={`https://cdn.discordapp.com/app-assets/${activity.activities[anum].application_id}/${activity.activities[anum].assets!.large_image}.png`} layout="fill" />
                                             </div>
                                             {activity?.activities[0].assets!.small_image ? (
                                                 <div className="absolute bottom-[-5px] right-[-5px] min-w-[20px] h-[20px] rounded-[50%] bg-norm-gray/20 border-2 border-solid border-black/80 overflow-hidden">
-                                                    <Image alt={`${activity?.activities[0].name} Small Image`} src={`https://cdn.discordapp.com/app-assets/${activity.activities[0].application_id}/${activity.activities[0].assets!.small_image}.png`} layout="fill" />
+                                                    <Image alt={`${activity?.activities[anum].name} Small Image`} src={`https://cdn.discordapp.com/app-assets/${activity.activities[anum].application_id}/${activity.activities[anum].assets!.small_image}.png`} layout="fill" />
                                                 </div>
                                             ) : null}
                                         </div>
@@ -61,9 +65,9 @@ export const Lanyard = (props: any) => {
                                             </div>
                                         )}
                                         <div className="grow">
-                                            <h1 className="font-bold leading-none text-[16px]">{activity?.activities[0].name}</h1>
-                                            <p className="leading-none mt-[6px] text-[14px]">{activity?.activities[0].details}</p>
-                                            <p className="leading-none mt-[6px] text-[14px]">{activity?.activities[0].state}</p>
+                                            <h1 className="font-bold leading-none text-[16px]">{activity?.activities[anum].name}</h1>
+                                            <p className="leading-none mt-[6px] text-[14px]">{activity?.activities[anum].details}</p>
+                                            <p className="leading-none mt-[6px] text-[14px]">{activity?.activities[anum].state}</p>
                                         </div>
                                     </div>
                                 </div>
