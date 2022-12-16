fa98b2c40544a037831b61f4c5aac910dcc444e9
lina <me@jackli.dev>
2022-12-16 08:11:12 -0700

    feat: add hyme redirect

--- next.config.js
+++ next.config.js
@@ -30,6 +30,11 @@ module.exports = {
         source: "/picsiv",
         destination: "https://discord.com/oauth2/authorize?client_id=947361674703302738&scope=applications.commands%20bot&permissions=3072",
         permanent: false,
+      },
+      {
+        source: "/hyme",
+        destination: "https://discord.com/api/oauth2/authorize?client_id=1037633541263073291&permissions=36965376&scope=bot%20applications.commands",
+        permanent: false,
       }
     ]
   },
