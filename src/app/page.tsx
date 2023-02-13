6f9e4ca557cfca674de906719867c25a4bac0732
lina <me@jackli.dev>
2023-02-13 09:20:57 -0700

    style: change footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,39 +7,43 @@ import { Activity } from "@/components/Lanyard/Activity";
 const Index = () => {
     return (
         <>
-            <div className="w-full mt-40 flex flex-col items-center justify-center text-text-color">
-                <div className="max-w-[750px] px-10">
-                    <div>
-                        <Image
-                            src="https://avatars.githubusercontent.com/u/39673993"
-                            alt="jckli"
-                            width={100}
-                            height={100}
-                            className="rounded-full"
-                        />
-                    </div>
-                    <h1 className="mt-4 text-xl">
-                        Jack Li{" "}
-                        <a
-                            href="https://github.com/jckli"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="text-text-darkest transition-all ease-in-out transition-200 hover:text-pink-accent"
-                        >
-                            (@jckli)
-                        </a>
-                    </h1>
-                    <p className="font-metropolis text-lg">Python, TypeScript, Go. I make cool things using code.</p>
-                    <div className="mt-2 flex gap-3">
-                        <IconButton icon={faEnvelope} href="mailto:me@jackli.dev" />
-                        <IconButton icon={faGithub} href="https://github.com/jckli" />
-                        <IconButton icon={faLinkedin} href="https://www.linkedin.com/in/jackhli/" />
-                        <IconButton icon={faTwitter} href="https://twitter.com/notjackli" />
+            <div className="h-[80vh]">
+                <div className="w-full pt-40 flex flex-col items-center justify-center text-text-color">
+                    <div className="max-w-[750px] px-10">
+                        <div>
+                            <Image
+                                src="https://avatars.githubusercontent.com/u/39673993"
+                                alt="jckli"
+                                width={100}
+                                height={100}
+                                className="rounded-full"
+                            />
+                        </div>
+                        <h1 className="mt-4 text-xl">
+                            Jack Li{" "}
+                            <a
+                                href="https://github.com/jckli"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="text-text-darkest transition-all ease-in-out transition-200 hover:text-pink-accent"
+                            >
+                                (@jckli)
+                            </a>
+                        </h1>
+                        <p className="font-metropolis text-lg">
+                            Python, TypeScript, Go. I make cool things using code.
+                        </p>
+                        <div className="mt-2 flex gap-3">
+                            <IconButton icon={faEnvelope} href="mailto:me@jackli.dev" />
+                            <IconButton icon={faGithub} href="https://github.com/jckli" />
+                            <IconButton icon={faLinkedin} href="https://www.linkedin.com/in/jackhli/" />
+                            <IconButton icon={faTwitter} href="https://twitter.com/notjackli" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex items-center justify-center px-8 mt-20">
-                <Activity />
+                <div className="flex items-center justify-center px-8 mt-20">
+                    <Activity />
+                </div>
             </div>
         </>
     );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 export const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
-        <div className="absolute bottom-0 flex justify-center w-full text-text-color">
-            <div className="py-[30px] items-center flex flex-row justify-between xl:w-[1140px] lg:w-[960px] md:w-[720px] sm:w-[530px]">
+        <div className="flex justify-center w-full text-text-color h-[10vh]">
+            <div className="items-center flex flex-row justify-between xl:w-[1140px] lg:w-[960px] md:w-[720px] sm:w-[530px]">
                 <div id="first" className="text-norm-gray text-sm hidden sm:block">
                     Made with <FontAwesomeIcon icon={faHeart} /> by{" "}
                     <a
