4426f3ceb2af596c2a4b5298ef99b68f3214b4f4
lina <me@jackli.dev>
2023-02-13 11:28:21 -0700

    fix (pt2): forgot something lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,8 @@ const Index = () => {
                 <div className="flex items-center justify-center px-8 mt-20">
                     <Activity />
                 </div>
-                <Footer />
             </div>
+            <Footer />
         </>
     );
 };
