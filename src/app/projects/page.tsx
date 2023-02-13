8a242cd4b93e89e6e0df9c48fe61a2dae40207fa
lina <me@jackli.dev>
2023-02-13 11:21:29 -0700

    feat: add links to projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -16,24 +16,34 @@ const Projects = () => {
                                 src="/projects/mangaupdates-bot.png"
                                 lang="Python"
                                 description="Track your favorite mangas, manhwas, or doujins and get every new chapter update sent to you!"
+                                button_link="https://tsuuchi.hayasaka.moe/"
+                                button_text="Invite"
+                                github_link="https://github.com/jckli/mangaupdates-bot"
                             />
                             <Project
                                 name="Picsiv"
                                 src="/projects/picsiv.png"
                                 lang="Python"
                                 description="Enhance your Discord experience with the ability to view full images from Pixiv and other art-related features!"
+                                button_link="https://picsiv.hayasaka.moe/"
+                                button_text="Invite"
+                                github_link="https://github.com/jckli/picsiv/"
                             />
                             <Project
                                 name="valorant.go"
                                 src="/projects/valorant.png"
                                 lang="Go"
                                 description="Access the unofficial Valorant API with ease using this user-friendly, efficient, Golang API wrapper."
+                                github_link="https://github.com/jckli/valorant.go/"
                             />
                             <Project
                                 name="cheinstein.py"
                                 src="/projects/cheinstein.png"
                                 lang="Python"
                                 description="Quickly extract and parse questions and answers from Chegg.com, all within a python library."
+                                button_link="https://pypi.org/project/cheinstein.py/"
+                                button_text="PyPI"
+                                github_link="https://github.com/DouglasTaylorSupportGroup/cheinstein.py"
                             />
                         </div>
                         <div className="flex flex-col gap-5">
@@ -42,24 +52,36 @@ const Projects = () => {
                                 src="/projects/retrievify.png"
                                 lang="TypeScript, Python"
                                 description="Get insights into all your listening statistics and see how you've spent your time listening to music."
+                                button_link="https://retrievify.hayasaka.moe/"
+                                button_text="Visit"
+                                github_link="https://github.com/jckli/statsify"
                             />
                             <Project
                                 name="SugoiArt"
                                 src="https://avatars.githubusercontent.com/u/104050203"
                                 lang="Go"
                                 description="A simple, fast, and open source anime art API completely hosted using GitHub repositories."
+                                button_link="https://art.hayasaka.moe/"
+                                button_text="Visit"
+                                github_link="https://github.com/sugoiart/art"
                             />
                             <Project
                                 name="Hyme"
                                 src="/projects/hyme.png"
                                 lang="Go"
                                 description="Enjoy music effortlessly from YouTube, Spotify, Soundcloud, and Bandcamp using Hyme."
+                                button_link="https://jackli.dev/hyme"
+                                button_text="Invite"
+                                github_link="https://github.com/jckli/hyme"
                             />
                             <Project
                                 name="Aidoku Sources"
                                 src="https://avatars.githubusercontent.com/u/97767528"
                                 lang="Rust"
-                                description="I develop some sources for this manga reader app, Aidoku. It's Tachiyomi but for iOS. Check it out!"
+                                description="I develop some sources for the manga reader app, Aidoku. It's Tachiyomi but for iOS. (I don't own this)"
+                                button_link="https://aidoku.app/"
+                                button_text="Visit"
+                                github_link="https://github.com/Skittyblock/aidoku-community-sources"
                             />
                         </div>
                     </div>
@@ -70,48 +92,70 @@ const Projects = () => {
                             src="/projects/mangaupdates-bot.png"
                             lang="Python"
                             description="Track your favorite mangas, manhwas, or doujins and get every new chapter update sent to you!"
+                            button_link="https://tsuuchi.hayasaka.moe/"
+                            button_text="Invite"
+                            github_link="https://github.com/jckli/mangaupdates-bot"
                         />
                         <Project
                             name="Retrievify"
                             src="/projects/retrievify.png"
                             lang="TypeScript, Python"
                             description="Get insights into all your listening statistics and see how you've spent your time listening to music."
+                            button_link="https://retrievify.hayasaka.moe/"
+                            button_text="Visit"
+                            github_link="https://github.com/jckli/statsify"
                         />
                         <Project
                             name="Picsiv"
                             src="/projects/picsiv.png"
                             lang="Python"
                             description="Enhance your Discord experience with the ability to view full images from Pixiv and other art-related features!"
+                            button_link="https://picsiv.hayasaka.moe/"
+                            button_text="Invite"
+                            github_link="https://github.com/jckli/picsiv/"
                         />
                         <Project
                             name="SugoiArt"
                             src="https://avatars.githubusercontent.com/u/104050203"
                             lang="Go"
                             description="A simple, fast, and open source anime art API completely hosted using GitHub repositories."
+                            button_link="https://art.hayasaka.moe/"
+                            button_text="Visit"
+                            github_link="https://github.com/sugoiart/art"
                         />
                         <Project
                             name="valorant.go"
                             src="/projects/valorant.png"
                             lang="Go"
                             description="Access the unofficial Valorant API with ease using this user-friendly, efficient, Golang API wrapper."
+                            github_link="https://github.com/jckli/valorant.go/"
                         />
                         <Project
                             name="Hyme"
                             src="/projects/hyme.png"
                             lang="Go"
                             description="Enjoy music effortlessly from YouTube, Spotify, Soundcloud, and Bandcamp using Hyme."
+                            button_link="https://jackli.dev/hyme"
+                            button_text="Invite"
+                            github_link="https://github.com/jckli/hyme"
                         />
                         <Project
                             name="cheinstein.py"
                             src="/projects/cheinstein.png"
                             lang="Python"
                             description="Quickly extract and parse questions and answers from Chegg.com, all within a python library."
+                            button_link="https://pypi.org/project/cheinstein.py/"
+                            button_text="PyPI"
+                            github_link="https://github.com/DouglasTaylorSupportGroup/cheinstein.py"
                         />
                         <Project
                             name="Aidoku Sources"
                             src="https://avatars.githubusercontent.com/u/97767528"
                             lang="Rust"
-                            description="I develop some sources for this manga reader app, Aidoku. It's Tachiyomi but for iOS. Check it out!"
+                            description="I develop some sources for the manga reader app, Aidoku. It's Tachiyomi but for iOS. (I don't own this)"
+                            button_link="https://aidoku.app/"
+                            button_text="Visit"
+                            github_link="https://github.com/Skittyblock/aidoku-community-sources"
                         />
                     </div>
                 )}
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const Project = (props: any) => {
     return (
@@ -21,10 +25,33 @@ export const Project = (props: any) => {
                                 <p className="text-sm text-text-darker leading-6">{props.lang}</p>
                             </div>
                         </div>
-                        <div>
-                            <p className="mt-4 font-metropolis text-base text-text-darker leading-6">
-                                {props.description}
-                            </p>
+                        <div className="mt-4">
+                            <p className="font-metropolis text-base text-text-darker leading-6">{props.description}</p>
+                        </div>
+                        <div className="mt-4 flex items-center gap-3">
+                            {props.button_text && props.button_link && (
+                                <motion.a
+                                    href={props.button_link}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="
+                                    inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-text-color bg-[#2e3440] hover:bg-[#424a54] transition-all ease-in-out duration-200
+                                "
+                                    whileTap={{ scale: 0.95 }}
+                                >
+                                    <FontAwesomeIcon icon={faLink} />
+                                    <h1 className="ml-2">{props.button_text}</h1>
+                                </motion.a>
+                            )}
+                            <motion.a
+                                href={props.github_link}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="text-2xl text-text-color transition-all ease-in-out duration-200 hover:text-white/70 hover:cursor-pointer"
+                                whileTap={{ scale: 0.95 }}
+                            >
+                                <FontAwesomeIcon icon={faGithub} />
+                            </motion.a>
                         </div>
                     </div>
                 </div>
