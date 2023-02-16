acf30d0773acb6b0290f466313d7ca81a52a3ee9
lina <me@jackli.dev>
2023-02-16 10:45:13 -0700

    refactor: change into masonry for projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,164 +1,93 @@
 "use client";
 
 import { Project } from "@/components/Project";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
+import Masonry from "react-masonry-css";
+
+const breakpointColumnsObj = {
+    default: 2,
+    700: 1,
+};
 
 const Projects = () => {
-    const isBreakpoint = useMediaQuery("810px");
     return (
         <>
             <div className="text-text-color flex flex-col justify-center items-center">
-                {isBreakpoint ? (
-                    <div className="flex gap-5">
-                        <div className="flex flex-col gap-5">
-                            <Project
-                                name="MangaUpdates Bot"
-                                src="/projects/mangaupdates-bot.png"
-                                lang="Python"
-                                description="Track your favorite mangas, manhwas, or doujins and get every new chapter update sent to you!"
-                                button_link="https://tsuuchi.hayasaka.moe/"
-                                button_text="Invite"
-                                github_link="https://github.com/jckli/mangaupdates-bot"
-                            />
-                            <Project
-                                name="Picsiv"
-                                src="/projects/picsiv.png"
-                                lang="Python"
-                                description="Enhance your Discord experience with the ability to view full images from Pixiv and other art-related features!"
-                                button_link="https://picsiv.hayasaka.moe/"
-                                button_text="Invite"
-                                github_link="https://github.com/jckli/picsiv/"
-                            />
-                            <Project
-                                name="valorant.go"
-                                src="/projects/valorant.png"
-                                lang="Go"
-                                description="Access the unofficial Valorant API with ease using this user-friendly, efficient, Golang API wrapper."
-                                github_link="https://github.com/jckli/valorant.go/"
-                            />
-                            <Project
-                                name="cheinstein.py"
-                                src="/projects/cheinstein.png"
-                                lang="Python"
-                                description="Quickly extract and parse questions and answers from Chegg.com, all within a python library."
-                                button_link="https://pypi.org/project/cheinstein.py/"
-                                button_text="PyPI"
-                                github_link="https://github.com/DouglasTaylorSupportGroup/cheinstein.py"
-                            />
-                        </div>
-                        <div className="flex flex-col gap-5">
-                            <Project
-                                name="Retrievify"
-                                src="/projects/retrievify.png"
-                                lang="TypeScript, Python"
-                                description="Get insights into all your listening statistics and see how you've spent your time listening to music."
-                                button_link="https://retrievify.hayasaka.moe/"
-                                button_text="Visit"
-                                github_link="https://github.com/jckli/statsify"
-                            />
-                            <Project
-                                name="SugoiArt"
-                                src="https://avatars.githubusercontent.com/u/104050203"
-                                lang="Go"
-                                description="A simple, fast, and open source anime art API completely hosted using GitHub repositories."
-                                button_link="https://art.hayasaka.moe/"
-                                button_text="Visit"
-                                github_link="https://github.com/sugoiart/art"
-                            />
-                            <Project
-                                name="Hyme"
-                                src="/projects/hyme.png"
-                                lang="Go"
-                                description="Enjoy music effortlessly from YouTube, Spotify, Soundcloud, and Bandcamp using Hyme."
-                                button_link="https://jackli.dev/hyme"
-                                button_text="Invite"
-                                github_link="https://github.com/jckli/hyme"
-                            />
-                            <Project
-                                name="Aidoku Sources"
-                                src="https://avatars.githubusercontent.com/u/97767528"
-                                lang="Rust"
-                                description="I develop some sources for the manga reader app, Aidoku. It's Tachiyomi but for iOS. (I don't own this)"
-                                button_link="https://aidoku.app/"
-                                button_text="Visit"
-                                github_link="https://github.com/Skittyblock/aidoku-community-sources"
-                            />
-                        </div>
-                    </div>
-                ) : (
-                    <div className="flex flex-col gap-5">
-                        <Project
-                            name="MangaUpdates Bot"
-                            src="/projects/mangaupdates-bot.png"
-                            lang="Python"
-                            description="Track your favorite mangas, manhwas, or doujins and get every new chapter update sent to you!"
-                            button_link="https://tsuuchi.hayasaka.moe/"
-                            button_text="Invite"
-                            github_link="https://github.com/jckli/mangaupdates-bot"
-                        />
-                        <Project
-                            name="Retrievify"
-                            src="/projects/retrievify.png"
-                            lang="TypeScript, Python"
-                            description="Get insights into all your listening statistics and see how you've spent your time listening to music."
-                            button_link="https://retrievify.hayasaka.moe/"
-                            button_text="Visit"
-                            github_link="https://github.com/jckli/statsify"
-                        />
-                        <Project
-                            name="Picsiv"
-                            src="/projects/picsiv.png"
-                            lang="Python"
-                            description="Enhance your Discord experience with the ability to view full images from Pixiv and other art-related features!"
-                            button_link="https://picsiv.hayasaka.moe/"
-                            button_text="Invite"
-                            github_link="https://github.com/jckli/picsiv/"
-                        />
-                        <Project
-                            name="SugoiArt"
-                            src="https://avatars.githubusercontent.com/u/104050203"
-                            lang="Go"
-                            description="A simple, fast, and open source anime art API completely hosted using GitHub repositories."
-                            button_link="https://art.hayasaka.moe/"
-                            button_text="Visit"
-                            github_link="https://github.com/sugoiart/art"
-                        />
-                        <Project
-                            name="valorant.go"
-                            src="/projects/valorant.png"
-                            lang="Go"
-                            description="Access the unofficial Valorant API with ease using this user-friendly, efficient, Golang API wrapper."
-                            github_link="https://github.com/jckli/valorant.go/"
-                        />
-                        <Project
-                            name="Hyme"
-                            src="/projects/hyme.png"
-                            lang="Go"
-                            description="Enjoy music effortlessly from YouTube, Spotify, Soundcloud, and Bandcamp using Hyme."
-                            button_link="https://jackli.dev/hyme"
-                            button_text="Invite"
-                            github_link="https://github.com/jckli/hyme"
-                        />
-                        <Project
-                            name="cheinstein.py"
-                            src="/projects/cheinstein.png"
-                            lang="Python"
-                            description="Quickly extract and parse questions and answers from Chegg.com, all within a python library."
-                            button_link="https://pypi.org/project/cheinstein.py/"
-                            button_text="PyPI"
-                            github_link="https://github.com/DouglasTaylorSupportGroup/cheinstein.py"
-                        />
-                        <Project
-                            name="Aidoku Sources"
-                            src="https://avatars.githubusercontent.com/u/97767528"
-                            lang="Rust"
-                            description="I develop some sources for the manga reader app, Aidoku. It's Tachiyomi but for iOS. (I don't own this)"
-                            button_link="https://aidoku.app/"
-                            button_text="Visit"
-                            github_link="https://github.com/Skittyblock/aidoku-community-sources"
-                        />
-                    </div>
-                )}
+                <Masonry
+                    breakpointCols={breakpointColumnsObj}
+                    className="flex w-auto smd:ml-[-1.5rem] gap-6 px-6"
+                    columnClassName="smd:pl-6 flex flex-col gap-6"
+                >
+                    <Project
+                        name="MangaUpdates Bot"
+                        src="/projects/mangaupdates-bot.png"
+                        lang="Python"
+                        description="Track your favorite mangas, manhwas, or doujins and get every new chapter update sent to you!"
+                        button_link="https://tsuuchi.hayasaka.moe/"
+                        button_text="Invite"
+                        github_link="https://github.com/jckli/mangaupdates-bot"
+                    />
+                    <Project
+                        name="Retrievify"
+                        src="/projects/retrievify.png"
+                        lang="TypeScript, Python"
+                        description="Get insights into all your listening statistics and see how you've spent your time listening to music."
+                        button_link="https://retrievify.hayasaka.moe/"
+                        button_text="Visit"
+                        github_link="https://github.com/jckli/statsify"
+                    />
+                    <Project
+                        name="Picsiv"
+                        src="/projects/picsiv.png"
+                        lang="Python"
+                        description="Enhance your Discord experience with the ability to view full images from Pixiv and other art-related features!"
+                        button_link="https://picsiv.hayasaka.moe/"
+                        button_text="Invite"
+                        github_link="https://github.com/jckli/picsiv/"
+                    />
+                    <Project
+                        name="SugoiArt"
+                        src="https://avatars.githubusercontent.com/u/104050203"
+                        lang="Go"
+                        description="A simple, fast, and open source anime art API completely hosted using GitHub repositories."
+                        button_link="https://art.hayasaka.moe/"
+                        button_text="Visit"
+                        github_link="https://github.com/sugoiart/art"
+                    />
+                    <Project
+                        name="valorant.go"
+                        src="/projects/valorant.png"
+                        lang="Go"
+                        description="Access the unofficial Valorant API with ease using this user-friendly, efficient, Golang API wrapper."
+                        github_link="https://github.com/jckli/valorant.go/"
+                    />
+                    <Project
+                        name="Hyme"
+                        src="/projects/hyme.png"
+                        lang="Go"
+                        description="Enjoy music effortlessly from YouTube, Spotify, Soundcloud, and Bandcamp using Hyme."
+                        button_link="https://jackli.dev/hyme"
+                        button_text="Invite"
+                        github_link="https://github.com/jckli/hyme"
+                    />
+                    <Project
+                        name="cheinstein.py"
+                        src="/projects/cheinstein.png"
+                        lang="Python"
+                        description="Quickly extract and parse questions and answers from Chegg.com, all within a python library."
+                        button_link="https://pypi.org/project/cheinstein.py/"
+                        button_text="PyPI"
+                        github_link="https://github.com/DouglasTaylorSupportGroup/cheinstein.py"
+                    />
+                    <Project
+                        name="Aidoku Sources"
+                        src="https://avatars.githubusercontent.com/u/97767528"
+                        lang="Rust"
+                        description="I develop some sources for the manga reader app, Aidoku. It's Tachiyomi but for iOS. (I don't own this)"
+                        button_link="https://aidoku.app/"
+                        button_text="Visit"
+                        github_link="https://github.com/Skittyblock/aidoku-community-sources"
+                    />
+                </Masonry>
             </div>
         </>
     );
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,50 +9,48 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export const Project = (props: any) => {
     return (
         <>
-            <div className="px-6 mmlg:px-0">
-                <div className="bg-[#1a202c] rounded-lg">
-                    <div className="p-5 mmlg:max-w-[360px]">
-                        <div className="flex items-center">
-                            <Image
-                                alt={`${props.name} Image`}
-                                src={props.src}
-                                width={60}
-                                height={60}
-                                className="rounded-lg"
-                            />
-                            <div className="ml-4">
-                                <h1 className="font-bold">{props.name}</h1>
-                                <p className="text-sm text-text-darker leading-6">{props.lang}</p>
-                            </div>
+            <div className="bg-[#1a202c] rounded-lg">
+                <div className="p-5 mmlg:max-w-[360px]">
+                    <div className="flex items-center">
+                        <Image
+                            alt={`${props.name} Image`}
+                            src={props.src}
+                            width={60}
+                            height={60}
+                            className="rounded-lg"
+                        />
+                        <div className="ml-4">
+                            <h1 className="font-bold">{props.name}</h1>
+                            <p className="text-sm text-text-darker leading-6">{props.lang}</p>
                         </div>
-                        <div className="mt-4">
-                            <p className="font-metropolis text-base text-text-darker leading-6">{props.description}</p>
-                        </div>
-                        <div className="mt-4 flex items-center gap-3">
-                            {props.button_text && props.button_link && (
-                                <motion.a
-                                    href={props.button_link}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="
-                                    inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-text-color bg-[#2e3440] hover:bg-[#424a54] transition-all ease-in-out duration-200
-                                "
-                                    whileTap={{ scale: 0.95 }}
-                                >
-                                    <FontAwesomeIcon icon={faLink} />
-                                    <h1 className="ml-2">{props.button_text}</h1>
-                                </motion.a>
-                            )}
+                    </div>
+                    <div className="mt-4">
+                        <p className="font-metropolis text-base text-text-darker leading-6">{props.description}</p>
+                    </div>
+                    <div className="mt-4 flex items-center gap-3">
+                        {props.button_text && props.button_link && (
                             <motion.a
-                                href={props.github_link}
+                                href={props.button_link}
                                 target="_blank"
                                 rel="noreferrer"
-                                className="text-2xl text-text-color transition-all ease-in-out duration-200 hover:text-white/70 hover:cursor-pointer"
+                                className="
+                                    inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-text-color bg-[#2e3440] hover:bg-[#424a54] transition-all ease-in-out duration-200
+                                "
                                 whileTap={{ scale: 0.95 }}
                             >
-                                <FontAwesomeIcon icon={faGithub} />
+                                <FontAwesomeIcon icon={faLink} />
+                                <h1 className="ml-2">{props.button_text}</h1>
                             </motion.a>
-                        </div>
+                        )}
+                        <motion.a
+                            href={props.github_link}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-2xl text-text-color transition-all ease-in-out duration-200 hover:text-white/70 hover:cursor-pointer"
+                            whileTap={{ scale: 0.95 }}
+                        >
+                            <FontAwesomeIcon icon={faGithub} />
+                        </motion.a>
                     </div>
                 </div>
             </div>
