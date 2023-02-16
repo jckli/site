0dd191b16d053775568c2ecd2149c9fa2bfcbfe5
lina <me@jackli.dev>
2023-02-16 10:18:59 -0700

    style: mobile styling fixes

--- src/components/QA.tsx
+++ src/components/QA.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export const QA = (props: any) => {
     return (
         <>
-            <div className={`text-text-color bg-[#1a202c] rounded-lg min-h-52 ${props.cols}`}>
-                <div className="px-5 py-6 h-full flex flex-col justify-center">
+            <div className={`text-text-color bg-[#1a202c] rounded-lg min-h-[13rem] ${props.cols}`}>
+                <div className="px-5 py-5 h-full flex flex-col justify-center">
                     <h1 className="font-bold">{props.question}</h1>
                     <p className="mt-2 text-text-darker font-metropolis ">{props.answer}</p>
                 </div>
