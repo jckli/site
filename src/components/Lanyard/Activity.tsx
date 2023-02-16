53ec5d01caddf4aeb1f7d7b74e0d5d148b1400ea
lina <me@jackli.dev>
2023-02-15 19:54:02 -0700

    feat: add transition leave

--- src/components/Lanyard/Activity.tsx
+++ src/components/Lanyard/Activity.tsx
@@ -14,6 +14,9 @@ export const Activity = (props: any) => {
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
                 <div className="bg-[#1a202c] rounded-lg">
                     {data && data.activities.length > 0 && (
