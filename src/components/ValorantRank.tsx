031ccfcfcc4253755448963c18c4973383153eda
lina <me@jackli.dev>
2023-02-18 18:39:36 -0700

    feat: last game

--- src/components/ValorantRank.tsx
+++ src/components/ValorantRank.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(" ");
+}
+
 export async function ValorantRank(props: any) {
     const puuid = "32df10f8-f9c5-5d39-a9b2-f811357ae4b1";
     const data = await getValorantPlayerData(puuid);
@@ -7,6 +11,7 @@ export async function ValorantRank(props: any) {
 
     const currentPlayerRankData =
         ranks.data[ranks.data.length - 1].tiers[data.data.LatestCompetitiveUpdate.TierAfterUpdate];
+    const rankedRatingEarned = data.data.LatestCompetitiveUpdate.RankedRatingEarned;
 
     return (
         <>
@@ -17,9 +22,23 @@ export async function ValorantRank(props: any) {
                 <Image alt="Valorant Rank Icon" src={currentPlayerRankData.largeIcon} width={96} height={96} />
                 <div className="mt-2 flex flex-col items-center justify-center">
                     <h1 className="text-lg leading-6">{currentPlayerRankData.tierName}</h1>
-                    <p className="font-metropolis leading-6">
-                        {data.data.LatestCompetitiveUpdate.RankedRatingAfterUpdate}/100
-                    </p>
+                    <div className="font-metropolis w-full flex flex-col justify-center items-center">
+                        <p className="leading-6">{data.data.LatestCompetitiveUpdate.RankedRatingAfterUpdate}/100</p>
+                        <p className="leading-6">
+                            last:{" "}
+                            <span
+                                className={
+                                    rankedRatingEarned > 0
+                                        ? "text-green-500"
+                                        : rankedRatingEarned < 0
+                                        ? "text-red-500"
+                                        : "text-text-color"
+                                }
+                            >
+                                {rankedRatingEarned > 0 ? `+${rankedRatingEarned}` : rankedRatingEarned}
+                            </span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
