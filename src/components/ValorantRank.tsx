2258f68114cab3687fda500b5e381b777db2f390
lina <me@jackli.dev>
2023-03-29 16:18:48 -0700

    feat: match stats

--- src/components/ValorantRank.tsx
+++ src/components/ValorantRank.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { ValorantSwitch } from "./ValorantSwitch";
+import { ValorantMatch } from "./ValorantMatch";
 
 function classNames(...classes: any) {
     return classes.filter(Boolean).join(" ");
@@ -8,36 +10,47 @@ export async function ValorantRank(props: any) {
     const puuid = "32df10f8-f9c5-5d39-a9b2-f811357ae4b1";
     const data = await getValorantPlayerData(puuid);
     const ranks = await getValorantRanks();
-
     const currentPlayerRankData =
         ranks.data[ranks.data.length - 1].tiers[data.data.LatestCompetitiveUpdate.TierAfterUpdate];
     const rankedRatingEarned = data.data.LatestCompetitiveUpdate.RankedRatingEarned;
+    const latestMatchId = data.data.LatestCompetitiveUpdate.MatchID;
 
     return (
         <>
             <div
-                className="h-full w-full flex flex-col items-center justify-center rounded-lg min-h-[13rem] text-white"
+                className="h-full w-full relative flex flex-col items-center justify-center rounded-lg min-h-[13rem] text-white"
                 style={{ backgroundColor: `#${currentPlayerRankData.backgroundColor.slice(0, -2)}` }}
             >
-                <Image alt="Valorant Rank Icon" src={currentPlayerRankData.largeIcon} width={96} height={96} />
-                <div className="mt-2 flex flex-col items-center justify-center">
-                    <h1 className="text-lg leading-6">{currentPlayerRankData.tierName}</h1>
-                    <div className="font-metropolis w-full flex flex-col justify-center items-center mt-1">
-                        <p className="leading-5">{data.data.LatestCompetitiveUpdate.RankedRatingAfterUpdate}/100</p>
-                        <p className="leading-4 text-sm">
-                            last:{" "}
-                            <span
-                                className={
-                                    rankedRatingEarned > 0
-                                        ? "text-green-500"
-                                        : rankedRatingEarned < 0
-                                        ? "text-red-500"
-                                        : "text-text-color"
-                                }
-                            >
-                                {rankedRatingEarned > 0 ? `+${rankedRatingEarned}` : rankedRatingEarned}
-                            </span>
-                        </p>
+                <ValorantSwitch>
+                    {/* @ts-expect-error Server Component */}
+                    <ValorantMatch
+                        backgroundColor={`#${currentPlayerRankData.backgroundColor.slice(0, -2)}`}
+                        matchId={latestMatchId}
+                        puuid={puuid}
+                        rrEarned={rankedRatingEarned}
+                    />
+                </ValorantSwitch>
+                <div className="flex flex-col items-center justify-center">
+                    <Image alt="Valorant Rank Icon" src={currentPlayerRankData.largeIcon} width={96} height={96} />
+                    <div className="mt-2 flex flex-col items-center justify-center">
+                        <h1 className="text-lg leading-6">{currentPlayerRankData.tierName}</h1>
+                        <div className="font-metropolis w-full flex flex-col justify-center items-center mt-1">
+                            <p className="leading-5">{data.data.LatestCompetitiveUpdate.RankedRatingAfterUpdate}/100</p>
+                            <p className="leading-4 text-sm">
+                                last:{" "}
+                                <span
+                                    className={
+                                        rankedRatingEarned > 0
+                                            ? "text-green-500"
+                                            : rankedRatingEarned < 0
+                                            ? "text-red-500"
+                                            : "text-text-color"
+                                    }
+                                >
+                                    {rankedRatingEarned > 0 ? `+${rankedRatingEarned}` : rankedRatingEarned}
+                                </span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/ValorantSwitch.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import { Transition } from "@headlessui/react";
+
+export const ValorantSwitch = ({ children }: any) => {
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => {
+        setShow(!show);
+    };
+
+    console.log(show);
+    return (
+        <>
+            <button onClick={handleShow} className="h-full w-full absolute rounded-md">
+                <div className="h-full w-full rounded-md">
+                    <Transition
+                        show={show}
+                        enter="transition-opacity duration-75 h-full w-full"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        entered="opacity-100 h-full w-full"
+                        leave="transition-opacity duration-200 h-full w-full"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        {children}
+                    </Transition>
+                </div>
+            </button>
+        </>
+    );
+};
