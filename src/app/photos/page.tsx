83bef040c0afab20c8d422e9b0ddd91c5f98cf53
lina <me@jackli.dev>
2023-07-16 02:33:57 -0700

    fix: update photo page api link

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -2,39 +2,39 @@ import Image from "next/image";
 import { PhotoClientComp } from "@/components/PhotoClientComp";
 
 const Photos = async () => {
-    const photos = await getPhotos();
-    return (
-        <>
-            <div className="flex flex-col justify-center items-center text-text-darker">
-                <h1 className="text-center text-sm px-10">All photos were taken by me, using a Canon Rebel T6.</h1>
-                <PhotoClientComp>
-                    {photos.files.map((photo: any) => (
-                        <div key={photo.id} className="relative">
-                            <Image
-                                className="object-contain rounded-md mb-6 w-full h-auto"
-                                src={photo.url}
-                                alt={photo.name}
-                                width="700"
-                                height="700"
-                                placeholder="blur"
-                                blurDataURL={photo.thumb}
-                            />
-                        </div>
-                    ))}
-                </PhotoClientComp>
-            </div>
-        </>
-    );
+	const photos = await getPhotos();
+	return (
+		<>
+			<div className="flex flex-col justify-center items-center text-text-darker">
+				<h1 className="text-center text-sm px-10">All photos were taken by me, using a Canon Rebel T6.</h1>
+				<PhotoClientComp>
+					{photos.files.map((photo: any) => (
+						<div key={photo.id} className="relative">
+							<Image
+								className="object-contain rounded-md mb-6 w-full h-auto"
+								src={photo.url}
+								alt={photo.name}
+								width="700"
+								height="700"
+								placeholder="blur"
+								blurDataURL={photo.thumb}
+							/>
+						</div>
+					))}
+				</PhotoClientComp>
+			</div>
+		</>
+	);
 };
 
 export default Photos;
 
 async function getPhotos() {
-    const res = await fetch("https://cdn.hayasaka.moe/api/album/camera", { next: { revalidate: 180 } });
-    if (!res.ok) {
-        throw new Error("Failed to fetch photos");
-    }
+	const res = await fetch("https://cdn.hayasaka.moe/api/album/3pvqr8/view", { next: { revalidate: 180 } });
+	if (!res.ok) {
+		throw new Error("Failed to fetch photos");
+	}
 
-    const a = res.json();
-    return a;
+	const a = res.json();
+	return a;
 }
