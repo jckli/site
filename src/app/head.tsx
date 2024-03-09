9066e09c9df1f4bd556670e84effa0902060f8e3
lina <me@jackli.dev>
2024-03-08 22:55:47 -0700

    feat: make new card for name

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
-	return (
-		<>
-			<title>ohashi</title>
-			<meta content="width=device-width, initial-scale=1" name="viewport" />
-			<link rel="icon" href="/favicon.ico" />
-		</>
-	);
+    return (
+        <>
+            <title>@jckli</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
 }
