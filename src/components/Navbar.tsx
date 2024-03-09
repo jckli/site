fc7578513bad1f1eb5b7fdaacb7bf9a21b3f77c8
lina <me@jackli.dev>
2024-03-09 02:31:24 -0700

    feat: make navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,21 +10,36 @@ import {
     navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Navbar = (props: any) => {
+    const pathname = usePathname();
+
     return (
         <>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem className="">
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                Documentation
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
+            <div className="flex-grow py-2 px-2 border-[1px] border-borcol rounded-xl">
+                <NavigationMenu>
+                    <NavigationMenuList className="text-text-color font-metropolis-bold">
+                        <NavigationMenuItem>
+                            <Link href="/" legacyBehavior passHref>
+                                <NavigationMenuLink active={"/" === pathname} className={navigationMenuTriggerStyle()}>
+                                    Home
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href="/projects" legacyBehavior passHref>
+                                <NavigationMenuLink
+                                    active={"/projects" === pathname}
+                                    className={navigationMenuTriggerStyle()}
+                                >
+                                    Projects
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
         </>
     );
 };
