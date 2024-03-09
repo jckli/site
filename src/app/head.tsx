c3d3c5c9ada4eded90224f70ea1a4fec2b511ebc
lina <me@jackli.dev>
2024-03-08 21:54:14 -0700

    feat: remove all v4

--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-    return (
-        <>
-            <title>@jckli</title>
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
-            <link rel="icon" type="image/png" href="https://avatars.githubusercontent.com/u/39673993" />
-        </>
-    );
-}
