4437cea735fb00365b14be9eb8f02396c935fcc4
jckli <me@jackli.dev>
2025-11-01 23:58:31 +0000

    chore: add noreferrer

--- src/components/InlineLink.tsx
+++ src/components/InlineLink.tsx
@@ -3,6 +3,7 @@ export const InlineLink = (props: any) => {
 		<a
 			href={props.href}
 			target="_blank"
+			rel="noreferrer"
 			className="
             text-text-darker underline transition-all ease-in-out transition-200 hover:text-pink-accent
             "
